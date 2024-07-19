3efb7922e55d12d35fc3ce7e7cc80a01243afbbb
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-19 23:28:43 +0500

    Update simple_quiz_game.py

--- simple_quiz_game.py
+++ simple_quiz_game.py
@@ -1,44 +1,44 @@
             # checkout "README.md file" for project info
 
 # Questions, choices and answers are in separate variables
-question1 = "Who is known as the father of the computer?"
-choice1 = " GoslingA) Dennis Ritchie\nB) Bill Gates\nC) Charles Babbage\nD) James"
+question1 = "What is the syntax to import a module in python?"
+choice1 = " A)include\nB) required\nC) import\nD) load"
 answer1 = "C"
 
-question2 = "What is the largest planet in our Solar System?"
-choice2 = "A) Earth\nB) Jupiter\nC) Mars\nD) Saturn"
+question2 = "What is the purpose of the def keyword in python?"
+choice2 = "A) To define a variable\nB) To define a function\nC) To define a class\nD) To define a module"
 answer2 = "B"
 
-question3 = "Mitochondria is the power house of?"
-choice3 = "A) Golgi apparatus\nB) Cell\nC) Lysosomes\nD) Endoplasmic reticulum"
+question3 = "How do you comment out a line of code in python?"
+choice3 = "A) Using //\nB) Using #\nC) Using <--!\nD) Using */"
 answer3 = "B"
 
-question4 = "How many days are there in a leap year?"
-choice4 = "A) 28\nB) 365\nC) 366\nD) 364"
+question4 = "How do you concatenate two strings in python?"
+choice4 = "A) Using -\nB) Using *\nC) Using +\nD) Using /"
 answer4 = "C"
 
-question5 = "What is the brain of a computer system called?"
-choice5 = "A) CPU\nB) RAM\nC) GPU\nD) None of the above"
+question5 = "What is the purpose of the range() function in python?"
+choice5 = "A) To generate a sequence of number\nB) To generate a number\nC) To generate a string\nD) None of the above"
 answer5 = "A"
 
-question6 = "What does CPU stand for?"
-choice6 = "A) Computer Programming Unit\nB) Control Processing Unit\nC) Computer Processing Unit\nD) Central Processing Unit"
+question6 = "What is the data type of variable x in the code x = (1,2,3)?"
+choice6 = "A) Tuple\nB) Dictionary\nC) Set\nD) List"
 answer6 = "D"
 
-question7 = "What is the chemical symbol for gold?"
-choice7 = "A) Ag\nB) Au\nC) Fe\nD) Hg"
+question7 = "What is the break statement in python?"
+choice7 = "A) A statement that ask the users for input\nB) A statement that exists a loop\nC) A statement that prints output\nD) All of the above"
 answer7 = "B"
 
-question8 = "How many months have 28 days?"
-choice8 = "A) One\nB) Four\nC) Two\nD) Twelve"
+question8 = "What is the purpose of blackslash n in python?"
+choice8 = "A) To do nothing\nB) To reversed the string\nC) To split the string\nD) To break the line"
 answer8 = "D"
 
-question9 = "What is the largest ocean on Earth?"
-choice9 = "A) Pacific Ocean\nB) Indian Ocean\nC) Arctic Ocean\nD) Atlantic Ocean"
+question9 = "What is a string in python?"
+choice9 = "A) A sequence of characters\nB) A sequence of bookeans\nC) A sequence of numbers\nD) A sequence of lists"
 answer9 = "A"
 
-question10 = "Which disease is caused by a vitamin C deficiency?"
-choice10 = "A) Rickets\nB) Measles\nC) Scurvy\nD) Malaria"
+question10 = "What is a variable in python?"
+choice10 = "A) A value that can't be changed\nB) A way to asks a user for input\nC) A name given to value\nD) A statement to print output"
 answer10 = "C"
 
 #Variable to track score
